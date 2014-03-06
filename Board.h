4c86989b84084667aa8b029b2edf9fbc12e5b9e9
Sean Howard <showard3@nd.edu>
2014-03-05 19:35:54 -0500

    First instance of Board header

---
+++ Board.h
@@ -0,0 +1,63 @@
+////////////////////////////////////////////////////////////////
+//Header for the Board Class to be used in the ND Civilization game
+//
+//FUND 2 Final Project 
+//
+//Sean Howard and Nick Sayre
+/////////////////////////////////////////////////////////////////
+
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+
+
+class Board:
+{
+
+	public:
+		//Displays the board to the screen. Need to use SDL eventually
+		void display();
+
+
+
+
+	private:
+		//2D vector to store various units, i.e. buildings, students, etc.
+		//to be displayed to the screen
+		vector<vector <Unit> >
+
+
+
+
+
+
+
+
+
+
+};
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
